230c700999bacfc402f349dd66ad2c0526122dab
bookshibgithub <106967264+bookshibgithub@users.noreply.github.com>
2022-06-14 15:11:58 +0800

    Update bookshib.js

--- js/bookshib.js
+++ js/bookshib.js
@@ -1,1 +1,19 @@
+/*        if(){
 
+        }
+        */
+// Get HTML head element
+        var head = document.getElementsByTagName('HEAD')[0];
+ 
+        // Create new link Element
+        var link = document.createElement('link');
+ 
+        // set the attributes for link element
+        link.rel = 'stylesheet';
+     
+        link.type = 'text/css';
+     
+        link.href = 'bookshib.css';
+ 
+        // Append link element to HTML head
+        head.appendChild(link);
